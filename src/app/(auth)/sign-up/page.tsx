17dac3758ba4d20acc13f6f3b595f514fc2091ff
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2025-02-14 20:15:42 +0900

    /sign-upに関するファイルを削除

--- src/app/(auth)/sign-up/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignUpForm from "./components/sign-up-form";
-
-export default async function Page() {
-  return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <SignUpForm />
-    </main>
-  )
-}
