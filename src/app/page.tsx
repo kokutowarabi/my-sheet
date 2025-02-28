21f73e6d0ac1fe5560506034081924ad8e5fa27c
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2025-02-28 20:59:31 +0900

    fix: /ページの背景色をbg-gray-100に変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,12 @@
 // @/app/page.tsx
 
-import Header from "@/components/header";
-import getSheets from "@/data/getSheets";
-import getUser from "@/data/getUser";
-import { authOptions } from "@/lib/auth-options";
-import { getServerSession } from "next-auth";
 import Link from "next/link";
-import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    redirect("/sign-in");
-  }
-
-  const userId = session.user.id;
-  const user = await getUser(userId);
-  if (!user) return;
-  const sheets = await getSheets(userId);
-
   return (
     <>
       <main
-        className="flex justify-center items-center flex-col gap-10 min-h-[100svh]"
+        className="flex justify-center items-center flex-col gap-10 min-h-[100svh] bg-gray-100"
       >
         {/* <Link className="" href={`/u/${userId}`}>
           <span>プロフィール</span>
