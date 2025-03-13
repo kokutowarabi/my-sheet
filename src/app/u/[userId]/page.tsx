4aaa117b9b42c2cabd802b59b6cba19e5e32f2ab
鈴木翔也 <suzukishoya@suzukishouyanoMacBook-Air.local>
2025-03-13 19:35:46 +0900

    fix: ユーザーページのpage.tsxとloading.tsxのレイアウトを修正

--- src/app/u/[userId]/page.tsx
+++ src/app/u/[userId]/page.tsx
@@ -25,19 +25,22 @@ export default async function UserPage({ params }: UserPageProps) {
   const sheets = await getSheets(userId);
 
   return (
-    <main className="flex justify-center items-center min-h-[100svh]">
-      <div className="flex flex-col items-center space-y-4">
+    <main className="flex flex-col justify-start min-h-[100svh] max-h-[100svh] px-10 pb-10">
+      <div className="flex gap-10 items-center justify-start w-full pt-20 pb-8 px-10">
+        <div className="rounded-full bg-pink-200 w-20 h-20" />
+        <span className="text-4xl">{user.userName}</span>
+      </div>
+      <div className="flex flex-col gap-10 items-start py-10 px-10 w-full h-[80svh] overflow-y-auto hover:bg-gray-100 rounded-md hidden-scrollbar transition">
         {sheets.map(sheet => (
           <Link
             key={sheet.id}
             href={`/s/${sheet.id}`}
-            className="hover:underline"
+            className="p-6 w-full rounded-md text-2xl bg-white hover:bg-gray-600 hover:text-white text-gray-600 font-bold transition"
           >
             {sheet.sheetName}
           </Link>
         ))}
       </div>
-      <span>{user.userName}</span>
     </main>
   )
 }
